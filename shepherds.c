2e5d88579731462d0146b46c417d52b66598c2f4
F1merhub <nezametdinovsamir@gmail.com>
2025-12-10 00:57:57 +0300

    final v2

--- shepherds.c
+++ shepherds.c
@@ -7,7 +7,7 @@
 
 #define DIR1 1
 #define DIR2 2
-#define MAX_THREADS 30
+#define MAX_THREADS 20
 
 typedef struct {
     int queue[MAX_THREADS];
@@ -17,7 +17,7 @@ typedef struct {
 typedef struct {
     int direction;
     int id;
-    void* bridge_ptr;
+    void* crossing_ptr;
 } ShepherdData;
 
 typedef struct {
@@ -27,22 +27,22 @@ typedef struct {
     // int* queue_array;
     int queue_array[MAX_THREADS];
     int hat_present;
-    bool bridge_busy;
+    bool crossing_busy;
     int current_dir;
 
     DirectionQueue dir1_queue;
     DirectionQueue dir2_queue;
-} Bridge;
+} Crossing;
 
-void print_queue(Bridge* bridge, int direction, int current_id) {
+void print_queue(Crossing* crossing, int direction, int current_id) {
     DirectionQueue* queue;
     const char* dir_name;
     
     if (direction == DIR1) {
-        queue = &bridge->dir1_queue;
+        queue = &crossing->dir1_queue;
         dir_name = "DIR1";
     } else {
-        queue = &bridge->dir2_queue;
+        queue = &crossing->dir2_queue;
         dir_name = "DIR2";
     }
     
@@ -67,7 +67,6 @@ void print_queue(Bridge* bridge, int direction, int current_id) {
     
     printf("\n");
 }
-
 
 void queue_init(DirectionQueue* q) {
     q->size = 0;
@@ -96,123 +95,119 @@ void queue_remove_front(DirectionQueue* q) {
     q->size--;
 }
 
-
-
-void bridge_init(Bridge* bridge) {   // TODO переименовать бридж
-    pthread_mutex_init(&bridge->mutex, NULL);
-    pthread_cond_init(&bridge->cond, NULL);
-    bridge->hat_present = 0;
-    bridge->bridge_busy = false;
-    bridge->current_dir = 0;
+void crossing_init(Crossing* crossing) {   // TODO переименовать бридж
+    pthread_mutex_init(&crossing->mutex, NULL);
+    pthread_cond_init(&crossing->cond, NULL);
+    crossing->hat_present = 0;
+    crossing->crossing_busy = false;
+    crossing->current_dir = 0;
 }
 
-void bridge_destroy(Bridge* bridge) {
-    pthread_mutex_destroy(&bridge->mutex);
-    pthread_cond_destroy(&bridge->cond);
+void crossing_destroy(Crossing* crossing) {
+    pthread_mutex_destroy(&crossing->mutex);
+    pthread_cond_destroy(&crossing->cond);
 }
 
-void dir1_cross(Bridge* bridge, int shepherd_id) { 
-
-    pthread_mutex_lock(&bridge->mutex);
-    queue_add(&bridge->dir1_queue, shepherd_id);
-    pthread_mutex_unlock(&bridge->mutex);
+void dir1_cross(Crossing* crossing, int shepherd_id) { 
+    pthread_mutex_lock(&crossing->mutex);
+    queue_add(&crossing->dir1_queue, shepherd_id);
+    pthread_mutex_unlock(&crossing->mutex);
     printf("DIR1-%d встает в очередь\n", shepherd_id);  // оповещаем и добавляем в id очередь
 
-    print_queue(bridge, DIR1, shepherd_id); // распечатка очереди
+    print_queue(crossing, DIR1, shepherd_id); // распечатка очереди
 
-    while (bridge->dir1_queue.queue[0] != shepherd_id){        // в условии сказано, что пастух идет класть шапку, если перед входом никого нет, даже если кто то есть на мосту
+    while (crossing->dir1_queue.queue[0] != shepherd_id){        // в условии сказано, что пастух идет класть шапку, если перед входом никого нет, даже если кто то есть на мосту
         printf("DIR1-%d ждет около моста\n", shepherd_id);
-        pthread_cond_wait(&bridge->cond, &bridge->mutex);  // ждет сигнал и захватывает
+        pthread_cond_wait(&crossing->cond, &crossing->mutex);  // ждет сигнал и захватывает
     }
 
     printf("DIR1-%d: иду один через мост...\n", shepherd_id);
     usleep(50000);
-    if (bridge->hat_present == 0) { // использую две шапки
-        bridge->hat_present = 1;
+    if (crossing->hat_present == 0) { // использую две шапки
+        crossing->hat_present = 1;
         printf("DIR1-%d Поставил шапку\n", shepherd_id);
     }
     else {
         printf("DIR1-%d Поставил вторую шапку\n", shepherd_id);
-        bridge->hat_present = 2;
+        crossing->hat_present = 2;
     }
     usleep(50000);
 
-    while (bridge->bridge_busy) {        // теперь ждем освобождения моста 
+    while (crossing->crossing_busy) {        // теперь ждем освобождения моста 
         printf("DIR1-%d ждет: на мосту стадо\n", shepherd_id);
-        pthread_cond_wait(&bridge->cond, &bridge->mutex);  
+        pthread_cond_wait(&crossing->cond, &crossing->mutex);  
     }
 
-    bridge->bridge_busy = true; 
-    bridge->current_dir = DIR1;
-    queue_remove_front(&bridge->dir1_queue);  // освобождаем место у входа и позволяем другому пастуху положить свою шапку и сами начинаем переход со стадом
+    crossing->crossing_busy = true; 
+    crossing->current_dir = DIR1;
+    queue_remove_front(&crossing->dir1_queue);  // освобождаем место у входа и позволяем другому пастуху положить свою шапку и сами начинаем переход со стадом
     printf("DIR1-%d начинает переход. Мост занят DIR1\n", shepherd_id);
-    pthread_mutex_unlock(&bridge->mutex);
-    pthread_cond_broadcast(&bridge->cond);
+    pthread_mutex_unlock(&crossing->mutex);
+    pthread_cond_broadcast(&crossing->cond);
 
     printf("DIR1-%d: веду стадо через мост...\n", shepherd_id);
     usleep(rand() % 400000 + 200000);
     
-    pthread_mutex_lock(&bridge->mutex);
-    bridge->bridge_busy = false;
-    bridge->current_dir = 0;
-    bridge->hat_present = bridge->hat_present - 1;  
+    pthread_mutex_lock(&crossing->mutex);
+    crossing->crossing_busy = false;
+    crossing->current_dir = 0;
+    crossing->hat_present = crossing->hat_present - 1;  
     printf("DIR1-%d завершил переход и забирает шапку\n", shepherd_id);
-    pthread_mutex_unlock(&bridge->mutex);
+    pthread_mutex_unlock(&crossing->mutex);
 
-    pthread_cond_broadcast(&bridge->cond);
-
+    pthread_cond_broadcast(&crossing->cond);
 }
-void dir2_cross(Bridge* bridge, int shepherd_id) {
 
-    pthread_mutex_lock(&bridge->mutex);         
-    queue_add(&bridge->dir2_queue, shepherd_id);
-    pthread_mutex_unlock(&bridge->mutex);
+void dir2_cross(Crossing* crossing, int shepherd_id) {
+    pthread_mutex_lock(&crossing->mutex);         
+    queue_add(&crossing->dir2_queue, shepherd_id);
+    pthread_mutex_unlock(&crossing->mutex);
     printf("DIR2-%d встает в очередь\n", shepherd_id);  
 
-    print_queue(bridge, DIR2, shepherd_id); // распечатка очереди
+    print_queue(crossing, DIR2, shepherd_id); // распечатка очереди
 
-    while (bridge->bridge_busy || bridge->hat_present || bridge->dir2_queue.queue[0] != shepherd_id) { // заходят, если нет шапок, мост не занят и они первые в очереди
-        if (bridge->dir2_queue.queue[0] == shepherd_id) {
-            if (bridge->hat_present)
+    while (crossing->crossing_busy || crossing->hat_present || crossing->dir2_queue.queue[0] != shepherd_id) { // заходят, если нет шапок, мост не занят и они первые в очереди
+        if (crossing->dir2_queue.queue[0] == shepherd_id) {
+            if (crossing->hat_present)
                 printf("DIR2-%d ждет: видит шапку\n", shepherd_id);
             else 
                 printf("SOMETHING WENT WRONG"); // т.к. после прохода сдвигаются id
         } else {
             printf("DIR2-%d ждет свою очередь\n", shepherd_id);
         }
-        pthread_cond_wait(&bridge->cond, &bridge->mutex);
+        pthread_cond_wait(&crossing->cond, &crossing->mutex);
     }
     
-    bridge->bridge_busy = true;
-    bridge->current_dir = DIR2;
-    pthread_mutex_unlock(&bridge->mutex);
+    crossing->crossing_busy = true;
+    crossing->current_dir = DIR2;
+    pthread_mutex_unlock(&crossing->mutex);
     
     printf("DIR2-%d начинает переход. Мост занят DIR2\n", shepherd_id);
     printf("DIR2-%d: веду стадо через мост...\n", shepherd_id);
     usleep(rand() % 500000 + 300000);
     
-    pthread_mutex_lock(&bridge->mutex);
-    bridge->bridge_busy = false;
-    bridge->current_dir = 0;
-    queue_remove_front(&bridge->dir2_queue);
+    pthread_mutex_lock(&crossing->mutex);
+    crossing->crossing_busy = false;
+    crossing->current_dir = 0;
+    queue_remove_front(&crossing->dir2_queue);
     printf("DIR2-%d завершил переход\n", shepherd_id);
-    pthread_mutex_unlock(&bridge->mutex);
+    pthread_mutex_unlock(&crossing->mutex);
 
-    pthread_cond_broadcast(&bridge->cond);
+    pthread_cond_broadcast(&crossing->cond);
 }
 
 void* shepherd_thread(void* arg) {
     ShepherdData* data = (ShepherdData*)arg;
     int direction = data->direction;
     int id = data->id;
-    Bridge* bridge = (Bridge*)data->bridge_ptr;
+    Crossing* crossing = (Crossing*)data->crossing_ptr;
     
     usleep(rand() % 1000000);
     
     if (direction == DIR1) {
-        dir1_cross(bridge, id);
+        dir1_cross(crossing, id);
     } else {
-        dir2_cross(bridge, id);
+        dir2_cross(crossing, id);
     }
     
     free(data);
@@ -222,15 +217,15 @@ void* shepherd_thread(void* arg) {
 int main() {
     srand(time(NULL));
     
-    Bridge bridge;
-    bridge_init(&bridge);
+    Crossing crossing;
+    crossing_init(&crossing);
     
     DirectionQueue queue1;
     DirectionQueue queue2;
     queue_init(&queue1);
     queue_init(&queue2);
-    (&bridge)->dir1_queue = queue1;
-    (&bridge)->dir2_queue = queue2;
+    (&crossing)->dir1_queue = queue1;
+    (&crossing)->dir2_queue = queue2;
 
     int num_shepherds = 10;
     pthread_t threads[num_shepherds];   // id потоков
@@ -242,7 +237,7 @@ int main() {
         ShepherdData* data = malloc(sizeof(ShepherdData));
         data->direction = (rand() % 2 == 0) ? DIR1 : DIR2;
         data->id = i + 1;
-        data->bridge_ptr = &bridge;
+        data->crossing_ptr = &crossing;
         data_array[i] = data;
         
         if (pthread_create(&threads[i], NULL, shepherd_thread, data) != 0) {
@@ -262,6 +257,6 @@ int main() {
     
     printf("\nВсе стада перешли через мост\n");
     
-    bridge_destroy(&bridge);
+    crossing_destroy(&crossing);
     return 0;
 }
